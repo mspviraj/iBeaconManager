75a46be8bd290e225cbd1d517cd6c6d7eda772c0
YoelL <yoellev8@gmail.com>
2017-05-21 12:01:59 +0300

    Blank Proj

---
+++ iBeaconManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconManager
+//
+//  Created by Yoel Lev on 5/21/17.
+//  Copyright © 2017 YoelL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
